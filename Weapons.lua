c315447bc68f16936ef438dc2de3e4ab928eebd3
OliverBinns1 <112949573+OliverBinns1@users.noreply.github.com>
2024-01-27 22:54:30 +0000

    Update Weapons.lua
    fixed to come under the correct tab function in the shops menu

--- Weapons.lua
+++ Weapons.lua
@@ -1,30 +1,30 @@
 Shop.Items["Base.AssaultRifle2"] = {
-	tab = Tab.Meds, price = 750, quantity = 1,
+	tab = Tab.Weapons, price = 750, quantity = 1,
 }
 Shop.Items["Base.M14Clip"] = {
-	tab = Tab.Meds, price = 150, quantity = 1,
+	tab = Tab.Weapons, price = 150, quantity = 1,
 }
 Shop.Items["Base.M16A2"] = {
-	tab = Tab.Meds, price = 1000, quantity = 1,
+	tab = Tab.Weapons, price = 1000, quantity = 1,
 }
 Shop.Items["Base.556Clip"] = {
-	tab = Tab.Meds, price = 100, quantity = 1,
+	tab = Tab.Weapons, price = 100, quantity = 1,
 }
 Shop.Items["Base.Pistol2"] = {
-	tab = Tab.Meds, price = 250, quantity = 1,
+	tab = Tab.Weapons, price = 250, quantity = 1,
 }
 Shop.Items["Base.45Clip"] = {
-	tab = Tab.Meds, price = 75, quantity = 1,
+	tab = Tab.Weapons, price = 75, quantity = 1,
 }
 Shop.Items["Base.AK47"] = {
-	tab = Tab.Meds, price = 1000, quantity = 1,
+	tab = Tab.Weapons, price = 1000, quantity = 1,
 }
 Shop.Items["Base.AK-47 Magazine"] = {
-	tab = Tab.Meds, price = 100, quantity = 1,
+	tab = Tab.Weapons, price = 100, quantity = 1,
 }
 Shop.Items["Base.SKS"] = {
-	tab = Tab.Meds, price = 750, quantity = 1,
+	tab = Tab.Weapons, price = 750, quantity = 1,
 }
 Shop.Items["Base.M60"] = {
-	tab = Tab.Meds, price = 10000, quantity = 1,
+	tab = Tab.Weapons, price = 10000, quantity = 1,
 }
