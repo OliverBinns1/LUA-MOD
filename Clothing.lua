df9d5ea0ceaef10147c0ea537b9adb510e6cc2de
OliverBinns1 <112949573+OliverBinns1@users.noreply.github.com>
2024-01-27 23:31:34 +0000

    Update Clothing.lua
    Update to the clothing section of the shop to add clothing options for purchase

--- Clothing.lua
+++ Clothing.lua
@@ -1,3 +1,57 @@
-Shop.Items["Base.Hunter_Mask_ON"] = {
-	tab = Tab.Clothing, price = 500, quantity = 1,
+Shop.Items["Base.Shoes_ArmyBoots"] = {
+	tab = Tab.Clothing, price = 100, quantity = 1,
+}
+Shop.Items["Base.Trousers_CamoGreen"] = {
+	tab = Tab.Clothing, price = 100, quantity = 1,
+}
+Shop.Items["Base.Jacket_ArmyCamoGreen"] = {
+	tab = Tab.Clothing, price = 100, quantity = 1,
+}
+Shop.Items["Base.Hat_Army"] = {
+	tab = Tab.Clothing, price = 150, quantity = 1,
+}
+Shop.Items["Base.Gloves_LeatherGlovesBlack"] = {
+	tab = Tab.Clothing, price = 75, quantity = 1,
+}
+Shop.Items["Base.Marpat_Jacket_W"] = {
+	tab = Tab.Clothing, price = 200, quantity = 1,
+}
+Shop.Items["Base.Marpat_Pants_W"] = {
+	tab = Tab.Clothing, price = 200, quantity = 1,
+}
+Shop.Items["Base.Killa_Jacket"] = {
+	tab = Tab.Clothing, price = 200, quantity = 1,
+}
+Shop.Items["Base.Killa_Pants"] = {
+	tab = Tab.Clothing, price = 200, quantity = 1,
+}
+Shop.Items["Base.Bag_AK_Vest"] = {
+	tab = Tab.Clothing, price = 250, quantity = 1,
+}
+Shop.Items["Base.Officer_Overcoat"] = {
+	tab = Tab.Clothing, price = 300, quantity = 1,
+}
+Shop.Items["Base.Glasses_Aviators"] = {
+	tab = Tab.Clothing, price = 50, quantity = 1,
+}
+Shop.Items["Base.Bandit_Jacket"] = {
+	tab = Tab.Clothing, price = 300, quantity = 1,
+}
+Shop.Items["Base.6B6_Helmet"] = {
+	tab = Tab.Clothing, price = 350, quantity = 1,
+}
+Shop.Items["Base.6B6_Visor"] = {
+	tab = Tab.Clothing, price = 200, quantity = 1,
+}
+Shop.Items["Base.Armor_Defender"] = {
+	tab = Tab.Clothing, price = 400, quantity = 1,
+}
+Shop.Items["Base.Gorka_Jacket_New"] = {
+	tab = Tab.Clothing, price = 100, quantity = 1,
+}
+Shop.Items["Base.Gorka_Pants_New"] = {
+	tab = Tab.Clothing, price = 100, quantity = 1,
+}
+Shop.Items["Base.GP5M_Gasmask"] = {
+	tab = Tab.Clothing, price = 50, quantity = 1,
 }
