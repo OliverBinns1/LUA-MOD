b9c92a5a8a874f09a43518f01f51df15b52adc6a
OliverBinns1 <112949573+OliverBinns1@users.noreply.github.com>
2024-01-27 23:07:14 +0000

    Update and rename t to Vehicles.lua
    replaced the redundant file with a vehicles sales tab containing 3 basic cars

---
+++ Vehicles.lua
@@ -0,0 +1,9 @@
+Shop.Items["PinkSlip.ModernCar"] = {
+	tab = Tab.Vehicles, price = 1000, quantity = 1,
+}
+Shop.Items["PinkSlip.PickUpVan"] = {
+	tab = Tab.Vehicles, price = 750, quantity = 1,
+}
+Shop.Items["PinkSlip.OffRoad"] = {
+	tab = Tab.Vehicles, price = 500, quantity = 1,
+}
