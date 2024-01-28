cc0363a99f850fb36a87f31c6c8eea4b917a3ee6
OliverBinns1 <112949573+OliverBinns1@users.noreply.github.com>
2024-01-28 02:10:39 +0000

    Update Weapons.lua
    fixed the poor formatting, and fixed the ak rifle magazine causing the show to not display it correctly

--- Weapons.lua
+++ Weapons.lua
@@ -1,30 +1,50 @@
 Shop.Items["Base.AssaultRifle2"] = {
-	tab = Tab.Weapons, price = 750, quantity = 1,
+    tab = Tab.Weapons,
+    price = 750,
+    quantity = 1
 }
 Shop.Items["Base.M14Clip"] = {
-	tab = Tab.Weapons, price = 150, quantity = 1,
+    tab = Tab.Weapons,
+    price = 150,
+    quantity = 1
 }
 Shop.Items["Base.M16A2"] = {
-	tab = Tab.Weapons, price = 1000, quantity = 1,
+    tab = Tab.Weapons,
+    price = 1000,
+    quantity = 1
 }
 Shop.Items["Base.556Clip"] = {
-	tab = Tab.Weapons, price = 100, quantity = 1,
+    tab = Tab.Weapons,
+    price = 100,
+    quantity = 1
 }
 Shop.Items["Base.Pistol2"] = {
-	tab = Tab.Weapons, price = 250, quantity = 1,
+    tab = Tab.Weapons,
+    price = 250,
+    quantity = 1
 }
 Shop.Items["Base.45Clip"] = {
-	tab = Tab.Weapons, price = 75, quantity = 1,
+    tab = Tab.Weapons,
+    price = 75,
+    quantity = 1
 }
 Shop.Items["Base.AK47"] = {
-	tab = Tab.Weapons, price = 1000, quantity = 1,
+    tab = Tab.Weapons,
+    price = 1000,
+    quantity = 1
 }
-Shop.Items["Base.AK-47 Magazine"] = {
-	tab = Tab.Weapons, price = 100, quantity = 1,
+Shop.Items["Base.AK_Mag"] = {
+    tab = Tab.Weapons,
+    price = 100,
+    quantity = 1
 }
 Shop.Items["Base.SKS"] = {
-	tab = Tab.Weapons, price = 750, quantity = 1,
+    tab = Tab.Weapons,
+    price = 750,
+    quantity = 1
 }
 Shop.Items["Base.M60"] = {
-	tab = Tab.Weapons, price = 10000, quantity = 1,
+    tab = Tab.Weapons,
+    price = 10000,
+    quantity = 1
 }
