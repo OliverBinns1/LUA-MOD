558ed7433617b7152ef399f0231527243ae73826
OliverBinns1 <112949573+OliverBinns1@users.noreply.github.com>
2024-01-28 18:49:33 +0000

    added food section for sale of food items

---
+++ Food.lua
@@ -0,0 +1,20 @@
+Shop.Items["CKRations.BouillonPowderSachet"] = {
+    tab = Tab.Food,
+    price = 25,
+    quantity = 1
+}
+Shop.Items["CKRations.CannedCRationBUnit"] = {
+    tab = Tab.Food,
+    price = 75,
+    quantity = 1
+}
+Shop.Items["CKRations.CannedCRationMUnit"] = {
+    tab = Tab.Food,
+    price = 75,
+    quantity = 1
+}
+Shop.Items["CKRations.KRation"] = {
+    tab = Tab.Food,
+    price = 100,
+    quantity = 1
+}
